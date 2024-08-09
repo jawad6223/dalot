2998a653747446d23aed034617cd010bf7fad80d
jawad6223 <jawad.bdo99@gmail.com>
2024-08-09 15:44:33 +0500

    field updated

--- styles/Field.js
+++ styles/Field.js
@@ -3,7 +3,7 @@ import { TextField } from "@mui/material";
 
 export const Field = styled(TextField)`
   background-color: #F9F9F9;
-  border-radius: 8px;
+  // border-radius: 8px;
 
   &:hover fieldset {
     border: none;
